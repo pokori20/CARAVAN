d6f9b10c339541f5baed4ffd64b8ea2e30757d57
EC2 Default User <ec2-user@ip-172-31-88-6.ec2.internal>
2021-12-08 07:47:27 +0000

    [finish]3-2

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resourcesでルーティングを設定するとよく使う7つのアクションのURLを自動で生成!
+  resources :blogs
 end
